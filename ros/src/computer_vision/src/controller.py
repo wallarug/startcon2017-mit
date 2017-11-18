9ddb51f9b34006c5c22838d6c49771f55edf88e5
William Xu <williamxuyuzhou@gmail.com>
2017-11-19 04:08:35 +1100

    adding controller to cv driving

---
+++ ros/src/computer_vision/src/controller.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+
+import rospy
+from ackermann_msgs.msg import AckermannDriveStamped
+from nav_msgs.msg import Path
+from std_msgs.msg import Int8
+from pid import PID
+
+from dynamic_reconfigure.server import Server
+from computer_vision.cfg import PIDParamsConfig
+
+class Controller:
+    def __init__(self):
+        rospy.init_node("path_controller")
+        self.debug = True
+
+        self.steering_pid = PID(0.5, 0.01, 0.001, -0.34, 0.34)
+
+
+        rospy.Subscriber("/navigation/waypoints", Path, self.on_path_received)
+        rospy.Subscriber("/vesc/ai", Int8, self.on_ai)
+
+        self.teleop_pub = rospy.Publisher('/vesc/low_level/ackermann_cmd_mux/input/teleop', AckermannDriveStamped, queue_size=10)
+
+        self.path = None
+        self.dt = 0.05 # difference in time between signals
+        self.last_ai_signal_received_time = None
+
+        srv = Server(PIDParamsConfig, self.config_callback)
+
+        rospy.spin()
+
+    def config_callback(self, config, level):
+        rospy.logwarn("Updating Steering PID %s, %s, %s", config["P"], config["I"], config["D"])
+        self.steering_pid.update_gains(config["P"], config["I"], config["D"])
+        return config
+
+    def on_path_received(self, data):
+        self.path = data.poses
+
+    def on_ai(self, data):
+        received_time = rospy.get_time()
+        # rospy.logwarn(received_time)
+
+        # keep track of time so we reset PID if it's been awhile
+        if self.last_ai_signal_received_time is not None:
+            if received_time - self.last_ai_signal_received_time > 1.:
+                rospy.logwarn("took %.2f seconds between last ai drive signal and now, resetting PID", received_time - self.last_ai_signal_received_time)
+                self.steering_pid.reset()
+        self.last_ai_signal_received_time = received_time
+
+        # if we've re
+        if self.path is not None:
+            self.publish_teleop()
+
+    def publish_teleop(self):
+        cte = self.path[0].pose.position.y
+        rospy.logwarn("first value (CTE): %s", cte)
+
+        steering = self.steering_pid.step(cte, self.dt)
+        stamp = self.generate_drive_stamp(steering)
+        self.teleop_pub.publish(stamp)
+
+
+    def generate_drive_stamp(self, steering):
+        stamp = AckermannDriveStamped()
+        stamp.drive.steering_angle = -steering
+        stamp.drive.speed = -0.3
+        rospy.logwarn("steering: %s", steering)
+        return stamp
+
+
+
+# header:
+#   seq: 1532
+#   stamp:
+#     secs: 0
+#     nsecs:         0
+#   frame_id: ''
+# drive:
+#   steering_angle: -0.34
+#   steering_angle_velocity: 0.0
+#   speed: 2.0
+#   acceleration: 0.0
+#   jerk: 0.0
+
+
+
+if __name__ == "__main__":
+    try:
+        Controller()
+    except rospy.ROSInterruptException:
+        rospy.logerr("Could not start Path Controller")
--- ros/src/computer_vision/src/detector.py
+++ ros/src/computer_vision/src/detector.py
@@ -4,25 +4,35 @@ import rospy
 import numpy as np
 from lane_detector import LaneDetector
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
 from cv_bridge import CvBridge, CvBridgeError
-
+from nav_msgs.msg import Path
 
 class Detector:
     def __init__(self):
         rospy.init_node("lane_detector")
-        self.detector = LaneDetector(debug=True)
+        self.debug = False
+        self.detector = LaneDetector(debug=self.debug)
         self.bridge = CvBridge()
         
         # subscribe to images
         rospy.Subscriber("left/image_rect_color", Image, self.on_left_image)
         rospy.Subscriber("right/image_rect_color", Image, self.on_right_image)
         
-        self.debug_pub = rospy.Publisher("debug/lane_image", Image, queue_size=1)
-        
-        
+        self.path_pub = rospy.Publisher('/navigation/waypoints', Path, queue_size=1, latch=True)
+
+        if self.debug == True:
+            self.debug_lane_pub = rospy.Publisher("debug/lane_image", Image, queue_size=1)
+            self.debug_filtered_left_pub = rospy.Publisher("debug/filtered_left", Image, queue_size=1)
+            self.debug_filtered_right_pub = rospy.Publisher("debug/filtered_right", Image, queue_size=1)
+            self.debug_warped_left_pub = rospy.Publisher("debug/warped_left", Image, queue_size=1)
+            self.debug_warped_right_pub = rospy.Publisher("debug/warped_right", Image, queue_size=1)
+            self.debug_clean_left_pub = rospy.Publisher("debug/clean_left", Image, queue_size=1)
+            self.debug_clean_right_pub = rospy.Publisher("debug/clean_right", Image, queue_size=1)
+
         self.left_image = None
         self.right_image = None
-        self.publish();
+        self.publish()
 
     def on_left_image(self, data):
         try:
@@ -37,19 +47,62 @@ class Detector:
             rospy.logerr(e)
 
     def publish(self):
-        rate = rospy.Rate(30)
+        rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            if self.left_image != None and self.right_image != None:
-                self.detector.process_images(self.left_image, self.right_image)
-                
-                debug_image = self.detector.show_lanes_as_image()
+            if self.left_image is not None and self.right_image is not None:
                 try:
-                    self.debug_pub.publish(self.bridge.cv2_to_imgmsg(debug_image, "rgb8"))
+                    # start = rospy.get_time()
+                    self.detector.process_images(self.left_image, self.right_image)
+                    # rospy.logwarn("time elapsed doing work: %s", rospy.get_time() - start)
+                    self.generate_path()
+                    if self.debug == True: self.debug_publish()
                 except CvBridgeError as e:
+                    rospy.logerr(e)
+                except Exception as e:
                     rospy.logerr(e)
         
             rate.sleep()
 
+    def debug_publish(self):
+        if self.debug == True:
+            debug_image = self.detector.debug_lanes_as_image()
+            self.debug_lane_pub.publish(self.bridge.cv2_to_imgmsg(debug_image, "rgb8"))
+            # left_filtered, right_filtered = self.detector.debug_filtered()
+            # self.debug_filtered_left_pub.publish(self.bridge.cv2_to_compressed_imgmsg(left_filtered, "mono8"))
+            # self.debug_filtered_right_pub.publish(self.bridge.cv2_to_compressed_imgmsg(right_filtered, "mono8"))
+            # left_warped, right_warped = self.detector.debug_warped()
+            # self.debug_warped_left_pub.publish(self.bridge.cv2_to_compressed_imgmsg(left_warped, "rgb8"))
+            # self.debug_warped_right_pub.publish(self.bridge.cv2_to_compressed_imgmsg(right_warped, "rgb8"))
+            # left_clean, right_clean = self.detector.debug_clean()
+            # self.debug_clean_left_pub.publish(s   elf.bridge.cv2_to_compressed_imgmsg(left_clean, "rgb8"))
+            # self.debug_clean_right_pub.publish(self.bridge.cv2_to_compressed_imgmsg(right_clean, "rgb8"))
+
+    def generate_path(self):
+        path = Path()
+        path.header.frame_id = "/zed_initial_frame"
+        centre, x_axis = self.detector.get_path()
+        # rospy.logwarn(centre)
+        # rospy.logwarn(x_axis)
+        for index, point in enumerate(centre):
+            pose = self.generate_pose(index/1000, point/1000, 0, 0, 0, 0, 0)  # ~ measuring a pixel per mm
+            path.poses.append(pose)
+            # just cutting this short to save on cpu time
+            if index > 5:
+                return
+
+        self.path_pub.publish(path)
+
+    def generate_pose(self, px, py, pz, ox, oy, oz, ow):
+        pose = PoseStamped()
+        # pose.header.frame_id = "/zed_initial_frame"
+        pose.pose.position.x = px
+        pose.pose.position.y = py
+        pose.pose.position.z = pz
+        pose.pose.orientation.x = ox
+        pose.pose.orientation.y = oy
+        pose.pose.orientation.z = oz
+        pose.pose.orientation.w = ow
+        return pose
 
 if __name__ == "__main__":
     try:
