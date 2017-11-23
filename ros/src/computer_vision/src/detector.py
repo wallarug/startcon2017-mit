f67f9e8c04d79d50f5bb35e17996c9b1a7576b62
William Xu <williamxuyuzhou@gmail.com>
2017-11-24 03:26:57 +1100

    changing to use only left camera

--- ros/src/computer_vision/src/detector.py
+++ ros/src/computer_vision/src/detector.py
@@ -11,24 +11,15 @@ from nav_msgs.msg import Path
 class Detector:
     def __init__(self):
         rospy.init_node("lane_detector")
-        self.debug = False
-        self.detector = LaneDetector(debug=self.debug)
+        self.detector = LaneDetector()
         self.bridge = CvBridge()
         
         # subscribe to images
         rospy.Subscriber("left/image_rect_color", Image, self.on_left_image)
-        rospy.Subscriber("right/image_rect_color", Image, self.on_right_image)
-        
+
         self.path_pub = rospy.Publisher('/navigation/waypoints', Path, queue_size=1, latch=True)
 
-        if self.debug == True:
-            self.debug_lane_pub = rospy.Publisher("debug/lane_image", Image, queue_size=1)
-            self.debug_filtered_left_pub = rospy.Publisher("debug/filtered_left", Image, queue_size=1)
-            self.debug_filtered_right_pub = rospy.Publisher("debug/filtered_right", Image, queue_size=1)
-            self.debug_warped_left_pub = rospy.Publisher("debug/warped_left", Image, queue_size=1)
-            self.debug_warped_right_pub = rospy.Publisher("debug/warped_right", Image, queue_size=1)
-            self.debug_clean_left_pub = rospy.Publisher("debug/clean_left", Image, queue_size=1)
-            self.debug_clean_right_pub = rospy.Publisher("debug/clean_right", Image, queue_size=1)
+        self.debug_lane_pub = rospy.Publisher("debug/lane_image", Image, queue_size=1)
 
         self.left_image = None
         self.right_image = None
@@ -40,22 +31,17 @@ class Detector:
         except CvBridgeError as e:
             rospy.logerr(e)
         
-    def on_right_image(self, data):
-        try:
-            self.right_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-        except CvBridgeError as e:
-            rospy.logerr(e)
 
     def publish(self):
         rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            if self.left_image is not None and self.right_image is not None:
+            if self.left_image is not None:
                 try:
                     # start = rospy.get_time()
-                    self.detector.process_images(self.left_image, self.right_image)
+                    target_line, debug_image = self.detector.process_images(self.left_image)
                     # rospy.logwarn("time elapsed doing work: %s", rospy.get_time() - start)
-                    self.generate_path()
-                    if self.debug == True: self.debug_publish()
+                    self.generate_path(target_line)
+                    self.debug_publish(debug_image)
                 except CvBridgeError as e:
                     rospy.logerr(e)
                 except Exception as e:
@@ -63,31 +49,17 @@ class Detector:
         
             rate.sleep()
 
-    def debug_publish(self):
-        if self.debug == True:
-            # debug_image = self.detector.debug_lanes_as_image()
-            # self.debug_lane_pub.publish(self.bridge.cv2_to_imgmsg(debug_image, "rgb8"))
-            # left_filtered, right_filtered = self.detector.debug_filtered()
-            # self.debug_filtered_left_pub.publish(self.bridge.cv2_to_compressed_imgmsg(left_filtered, "mono8"))
-            # self.debug_filtered_right_pub.publish(self.bridge.cv2_to_compressed_imgmsg(right_filtered, "mono8"))
-            # left_warped, right_warped = self.detector.debug_warped()
-            # self.debug_warped_left_pub.publish(self.bridge.cv2_to_compressed_imgmsg(left_warped, "rgb8"))
-            # self.debug_warped_right_pub.publish(self.bridge.cv2_to_compressed_imgmsg(right_warped, "rgb8"))
-            # left_clean, right_clean = self.detector.debug_clean()
-            # self.debug_clean_left_pub.publish(s   elf.bridge.cv2_to_compressed_imgmsg(left_clean, "rgb8"))
-            # self.debug_clean_right_pub.publish(self.bridge.cv2_to_compressed_imgmsg(right_clean, "rgb8"))
-            pass
+    def debug_publish(self, image):
+        self.debug_lane_pub.publish(self.bridge.cv2_to_imgmsg(image, "rgb8"))
+        pass
 
-    def generate_path(self):
+    def generate_path(self, target_line):
         path = Path()
         path.header.frame_id = "/zed_initial_frame"
-        centre, x_axis = self.detector.get_path()
-        # rospy.logwarn(centre)
-        # rospy.logwarn(x_axis)
-        for index, point in enumerate(centre):
+        rospy.logwarn(target_line[0])
+        for index, point in enumerate(target_line):
             pose = self.generate_pose(index/1000, point/1000, 0, 0, 0, 0, 0)  # ~ measuring a pixel per mm
             path.poses.append(pose)
-            # just cutting this short to save on cpu time
 
         self.path_pub.publish(path)
 
--- ros/src/computer_vision/src/lane_detector.py
+++ ros/src/computer_vision/src/lane_detector.py
@@ -3,6 +3,7 @@
 from scipy.ndimage.measurements import label
 import cv2 as cv2
 import numpy as np
+import sys
 import time
 
 """Lane Detector
@@ -10,32 +11,10 @@ Pass in [] into the constructor to receive a list of tuples for debugging purpos
 """
 
 class LaneDetector():
-    def __init__(self, debugger=None):
-        self.middle_of_car = (250 + 700) / 2
-        self.modified_width = 872
+    def __init__(self):
+        self.middle_of_car = (200 + 460)
+        self.modified_width = 1072
         self.modified_height = 376
-        self.debugger = debugger
-
-    def abs_sobel_thresh(self, hls, orient='x', sobel_kernel=3, thresh=(0, 255)):
-
-        # Convert to HLS and use lightness as the gray
-        L = hls[:, :, 1]
-        # 2) Take the derivative in x or y given orient = 'x' or 'y'
-        if orient == 'x':
-            sobel = cv2.Sobel(L, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
-        else:
-            sobel = cv2.Sobel(L, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
-        # 3) Take the absolute value of the derivative or gradient
-        abs_sobel = np.absolute(sobel)
-        # 4) Scale to 8-bit (0 - 255) then convert to type = np.uint8
-        scaled_sobel = np.uint8(255 * abs_sobel / np.max(abs_sobel))
-        # 5) Create a mask of 1's where the scaled gradient magnitude
-        # is > thresh_min and < thresh_max
-        sxbinary = np.zeros_like(scaled_sobel)
-        sxbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
-        # 6) Return this mask as your binary_output image
-        binary_output = sxbinary
-        return binary_output
 
     def hsl_channel_threshold(self, hls, l_thresh=(0., 50)):
         l_channel = hls[:, :, 1]
@@ -46,29 +25,27 @@ class LaneDetector():
 
         return l_binary
 
+    # not used yet
+    def colour_threshold(self, image, thresh=(210, 255)):
+        combined = np.zeros_like(image[:, :, 0])
+        combined[(image[:, :, 0] > thresh[0]) & (image[:, :, 1] > thresh[0]) & (image[:, :, 2] > thresh[0])
+                 & (image[:, :, 0] < thresh[1]) & (image[:, :, 1] < thresh[1]) & (image[:, :, 2] < thresh[1])] = 255
+        return combined
+
     def filter_lanes(self, image):
-        #         x_grad_binary = self.abs_sobel_thresh(image, orient='x', thresh=(20, 60))
-        #         y_grad_binary = self.abs_sobel_thresh(image, orient='y', thresh=(20, 60))
-        hsl_channel_binary = self.hsl_channel_threshold(image, l_thresh=(210., 255.))
+        hsl_channel_binary = self.hsl_channel_threshold(image, l_thresh=(0., 50.))
 
         combined = np.zeros_like(hsl_channel_binary)  # this makes it 1 channel
-        #         combined[(hsl_channel_binary == 1) & (x_grad_binary == 1) & (y_grad_binary == 1)] = 1
         combined[(hsl_channel_binary == 1)] = 1
         return combined
 
     def cut_image(self, image):
         return image[250:, :] #cut the top 250px
 
-    def warp_image(self, image, mode='normal', side="left"):
-        #         image = np.copy(image)
-        if side == "left":
-            warp_vertices_src = [(75, image.shape[0]), (300, 185), (385, 185), (597, image.shape[0])]  # both sides
-            src = np.float32(warp_vertices_src)
-            dst = np.float32([[375, image.shape[0]], [375, 0], [697, 0], [697, image.shape[0]]])
-        else:
-            warp_vertices_src = [(597, image.shape[0]), (447, 0), (336, 0), (336, image.shape[0])]
-            src = np.float32(warp_vertices_src)
-            dst = np.float32([[597, image.shape[0]], [597, 0], [336, 0], [336, image.shape[0]]])
+    def warp_image(self, image, mode='normal'):
+        warp_vertices_src = [(75, image.shape[0]), (300, 185), (385, 185), (597, image.shape[0])]  # both sides
+        src = np.float32(warp_vertices_src)
+        dst = np.float32([[375, image.shape[0]], [375, 0], [697, 0], [697, image.shape[0]]])
 
         if mode == 'normal':
             M = cv2.getPerspectiveTransform(src, dst)
@@ -78,36 +55,77 @@ class LaneDetector():
             warped = cv2.warpPerspective(image, M, (672, image.shape[0]), flags=cv2.INTER_LINEAR)
         return warped
 
-    def clean_image(self, image, side="left"):
-        # if side == "left":
-        #     image[:, -300:] = 0
-        #     image[-50:, -372:] = 0
-        # else:
-        #     image[:, 0:300] = 0
-        #     image[-50:, :450] = 0
-        return image  # all the stuff below takes too long
-        # labels = label(image)  # labels[0] = image with labels, labels[1] = count of labels
-        # result = labels[0]
-        #
-        # for object_no in range(1, labels[1] + 1):
-        #     # Find pixels with each label value
-        #     nonzero = (result == object_no).nonzero()
-        #     # Identify x and y values of those pixels
-        #     nonzeroy = np.array(nonzero[0])
-        #     nonzerox = np.array(nonzero[1])
-        #     if ((np.max(nonzeroy) - np.min(nonzeroy) < 5) & (np.max(nonzerox) - np.min(nonzerox) < 5)):
-        #         result[(result == object_no)] = 0
-        # result[result > 0] = 1
-        # return result
+    # returns lines, debug_image
+    def find_all_lines(self, image):
+        labeled_array, num_features = label(image)
 
-    def get_line_fit(self, points, y_axis):
-        y, x = np.nonzero(points)
-        fit = np.polyfit(y, x, 2)
-        line_fitx = fit[0] * y_axis ** 2 + fit[1] * y_axis + fit[2]
-        return line_fitx
+        image_height = image.shape[0]
+        y_axis = np.linspace(0, image_height - 1, num=image_height)
+
+
+        debug_image = np.zeros([image.shape[0], image.shape[1], 3], dtype=np.uint8)
+        debug_image[labeled_array > 0] = 255
+
+        lines = []
+
+        def get_line_fit(points, y_axis):
+            y, x = np.nonzero(points)
+            # ignore small blocks
+            if ((np.max(y) - np.min(y) < 100) & (np.max(x) - np.min(x) < 100)):
+                return None
+
+            fit = np.polyfit(y, x, 1)
+            # line_fitx = fit[0] * y_axis ** 2 + fit[1] * y_axis + fit[2]
+            line_fitx = fit[0] * y_axis + fit[1]
+            return line_fitx
+
+        for feature in range(1, num_features + 1):
+            feature_image = (labeled_array == feature)
+            try:
+                line_fit = get_line_fit(feature_image, y_axis)
+                if line_fit is not None:
+                    lines.append(line_fit)
+                    points = np.vstack((line_fit, y_axis)).T.astype(np.int32)
+                    cv2.polylines(debug_image, [points], False, (255, 0, 0), 3)
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+
+        # find target line
+        target_line = None
+        if len(lines) > 0:
+            left = np.zeros_like(lines[0])
+            left_count = 0
+            right = np.zeros_like(lines[0])
+            right_count = 0
+            for line in lines:  # total the lines to the left and right of the car separately
+                if line[0] < self.middle_of_car:
+                    left = left + line
+                    left_count = left_count + 1
+                if line[0] >= self.middle_of_car:
+                    right = right + line
+                    right_count = right_count + 1
+            if left_count == 0:
+                right = right / right_count  # average
+                left = right - 400  # this is a guess of lane width
+            elif right_count == 0:
+                left = left / left_count  # average
+                right = left + 400
+            else:
+                left = left / left_count  # average
+                right = right / right_count  # average
+            target_line = (left + right) / 2 # find middle of left and right
+        else:
+            target_line = np.full(image_height, self.middle_of_car)
+        points = np.vstack((target_line, y_axis)).T.astype(np.int32)
+        cv2.polylines(debug_image, [points], False, (0, 255, 0), 2)
+        cv2.line(debug_image, (self.middle_of_car, 0), (self.middle_of_car, image_height), (0, 0, 255), 2)
+
+        target_line = target_line - self.middle_of_car
+
+
+        return target_line, cv2.flip(debug_image, 0)
 
     def process_images(self, left):
-        # start = time.time()
         # left_cut = self.cut_image(left)
 
         # convert to HLS
@@ -115,91 +133,17 @@ class LaneDetector():
 
         # filter out lanes and warp it
         left_filtered = self.filter_lanes(left_image)
+        # left_filtered = self.colour_threshold(left)
 
         left_warped = self.warp_image(left_filtered)
 
+        left_flip = cv2.flip(left_warped, 0)
+
+        target_line, debug_image = self.find_all_lines(left_flip)
 
 
-        # clean and flip the image upside down to make the front of the car x = 0
-        # left_clean = self.clean_image(left_warped, side="left")
-        left_clean = left_warped
 
-        left_clean = cv2.flip(left_clean, 0)
-
-
-        # fit a line to the left and right
-        # image_height = left_clean.shape[0]
-        # y_axis = np.linspace(-50, image_height - 1, num=image_height + 50)
-        # y, x = np.nonzero(left_clean)
-        # left_fitx = []
-        # if len(y) > 0:
-        #     left_fitx = self.get_line_fit(left_clean, y_axis)
-        #
-        # # rospy.logwarn("time for lines 1: %s", rospy.get_time() - start)
-        # # start = time.time()
-        #
-        # # find centre line
-        # if len(right_fitx) == 0:
-        #     centre = left_fitx + self.middle_of_car / 2
-        # elif len(left_fitx) == 0:
-        #     centre = right_fitx - self.middle_of_car / 2
-        # elif len(left_fitx) > 0 and len(right_fitx) > 0:
-        #     centre = (left_fitx + right_fitx) / 2
-        # else:
-        #     centre = np.full(len(y_axis), self.middle_of_car)
-        #
-        # # translate this to have the front of the car to be the positive x-axis
-        # self.x_axis = y_axis
-        # if len(left_fitx) > 0:
-        #     self.left_y = (left_fitx - self.middle_of_car) * -1
-        # else:
-        #     self.left_y = []
-        # if len(right_fitx) > 0:
-        #     self.right_y = (right_fitx - self.middle_of_car) * -1
-        # else:
-        #     self.right_y = []
-        # self.centre = centre - self.middle_of_car
-
-        # rospy.logwarn("time for lines 2: %s", rospy.get_time() - start)
-        # start = time.time()
-
-        if self.debugger is not None:
-            # record images throughout
-            self.debugger.append(("input", left))
-            # self.debugger.append(("cut", [left_cut, right_cut]))
-            # warped_raw = self.warp_image(np.copy(left), side="left")
-            # warped_inverse_raw = self.warp_image(warped_raw, mode='inverse', side="left")
-            # self.debugger.append(("left_warp_raw", [warped_raw, warped_inverse_raw]))
-            # warped_raw = self.warp_image(np.copy(right), side="right")
-            # warped_inverse_raw = self.warp_image(warped_raw, mode='inverse', side="right")
-            # self.debugger.append(("right_warp_raw", [warped_raw, warped_inverse_raw]))
-
-            self.debugger.append(("filtered", self.build_image_from_mask(left_filtered)))
-            self.debugger.append(("warped", self.build_image_from_mask(left_warped)))
-            self.debugger.append(("clean", self.build_image_from_mask(cv2.flip(left_clean, 0))))
-
-            # image = np.zeros([image_height, self.modified_width, 3], dtype=np.uint8)
-            # y_axis = y_axis[51:][::-1]
-            # cv2.line(image, (self.middle_of_car, 0), (self.middle_of_car, image_height), (255, 255, 255), 2)
-            # if len(left_fitx) > 0:
-            #     points = np.vstack((left_fitx[51:], y_axis)).T.astype(np.int32)
-            #     cv2.polylines(image, [points], False, (255, 0, 0), 2)
-            # if len(right_fitx) > 0:
-            #     points = np.vstack((right_fitx[51:], y_axis)).T.astype(np.int32)
-            #     cv2.polylines(image, [points], False, (0, 0, 255), 2)
-
-            # points = np.vstack((centre[51:], y_axis)).T.astype(np.int32)
-            # cv2.polylines(image, [points], False, (0, 255, 0), 2)
-            # self.debugger.append(("lane-lines", [image]))
-
-        # rospy.logwarn("time for the end: %s", rospy.get_time() - start)
-
-    def get_path(self):
-        return self.centre, self.x_axis
-
-    def get_lanes(self):
-        # return self.left_y, self.right_y, self.x_axis
-        pass
+        return target_line, debug_image
 
     def build_image_from_mask(self, mask):
         image = np.zeros([mask.shape[0], mask.shape[1], 3], dtype=np.uint8)
