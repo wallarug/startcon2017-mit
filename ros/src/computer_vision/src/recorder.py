7f63835e0f0afffb53f621368a403470de28f7d9
Whathell <whathell@gmail.com>
2017-11-29 23:45:07 +1100

    fixing recorder

--- ros/src/computer_vision/src/recorder.py
+++ ros/src/computer_vision/src/recorder.py
@@ -39,9 +39,10 @@ class Recorder:
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             if self.left_image is not None and self.steer is not None and self.speed < 0:
+                randint = round(np.random.rand(), 5)
                 try:
                     rospy.logwarn("saving image with steer: %s, speed: %s", self.steer, self.speed)
-                    cv2.imwrite("../bagfiles/leftimage_" + str(self.steer) + ".jpg", self.left_image)
+                    cv2.imwrite("./" + str(randint) + "-leftimage_" + str(self.steer) + ".jpg", self.left_image)
                 except Exception as e:
                     rospy.logerr(e)
 
